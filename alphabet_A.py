0c9611cb4290a54e1726c61bad99f422e9151ec6
Nitish baghel <80021632+B39-nitishbaghel@users.noreply.github.com>
2021-03-18 17:31:39 +0530

    Add files via upload

---
+++ alphabet_A.py
@@ -0,0 +1,12 @@
+n=eval(input("enter the number of line"))
+for i in range(1,n+1):
+	for j  in range((n-i)+1,1,-1):
+		print(" ",end=" ")
+	for k in range(0,2*i-1):
+		if i==n-2:
+			print("*",end=" ")
+		elif k==0 or k==(2*i-1)-1:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
